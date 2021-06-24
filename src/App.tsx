6959c019f08d7e2ff530b8dc608fb1769b201bb3
AlanisMayumi <mayumi.alanis@gmail.com>
2021-06-24 20:51:34 -0300

    aula_01

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { createContext, useState, useEffect } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { firebase, auth } from "./services/Firebase";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" exact component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
